7f16e437358aac501c2dbc7522e055ad701c25c5
Rajczakowski <rajczakowski@Guests-MacBook-Pro.local>
2016-08-29 22:57:06 +0100

    Wiersze added.

---
+++ assets/js/single-pages/smoth_scroll.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(window).scroll(function() {
+  console.log($(this).scrollTop());
+    if ($(this).scrollTop() > 1500) {
+        $('.go-top').fadeIn();
+    } else {
+        $('.go-top').fadeOut();
+    }
+});
